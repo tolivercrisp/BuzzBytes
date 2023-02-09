e8e8b1f7e72eb5ae07e42d88afd55266d058ef3f
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-02-09 12:22:48 -0500

    Add express server connection code to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Prefix all routes with /api
 router.use('/api', apiRoutes);
 
+// If invalid route is used, send this
 router.use((req, res) => res.send('Wrong route!'));
 
 module.exports = router;
